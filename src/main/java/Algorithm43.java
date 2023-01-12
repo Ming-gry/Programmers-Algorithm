943fee16e7e1e25ae0b9b734561b465fc3ae0f34
Ming-gry <yby818@naver.com>
2023-01-12 11:46:11 +0900

    41-43

---
+++ src/main/java/Algorithm43.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class Algorithm43 {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++){
+            sum += d[i];
+            if (sum > budget){
+                answer = i;
+                break;
+            }
+        }
+
+        if (sum <= budget){
+            answer = d.length;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Algorithm43 algorithm43 = new Algorithm43();
+        int[] d = {1,3,2,5,4};
+
+        System.out.println(algorithm43.solution(d, 9));
+    }
+}
